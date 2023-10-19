e81a6ecd4ab14efe30e29ddbc59b7c2db73b4128
but-chantha <chanthabut2002@gmail.com>
2023-10-19 22:21:11 +0700

    javaScript

--- Javascript-lesson/Exercise/exercise6.js
+++ Javascript-lesson/Exercise/exercise6.js
@@ -1,17 +1,56 @@
+let hour = 18;
 
-let time;
-let hour = 17;
-
-if (hour => 6 && hour <= 12)
+if ( hour >= 6 && hour <= 12)
 {
-    time = `Good morning!`;
+    console.log(`Good morning!`);
 }
-else if (hour => 13 && hour <= 17)
+else if ( hour >= 13 && hour <= 17)
 {
-    time = `Good afternoon!`;
+    console.log(`Good afternoon!`);
 }
 else {
-    time = `Good night!`;
+    console.log(`Good night!`);
 }
 
-console.log(time);
+
+let hours = 18;
+
+if ( hours >= 6 && hours <= 12)
+{
+    console.log(`rice chicken!`);
+}
+else if ( hours >= 13 && hours <= 17)
+{
+    console.log(`fry rice!`);
+}
+else {
+    console.log(`BBQ!`);
+}
+
+const name = `chantha`;
+if ( hour >= 6 && hour <= 12)
+{
+    console.log(`Good morning ${name}!`);
+}
+else if ( hour >= 13 && hour <= 17)
+{
+    console.log(`Good afternoon ${name}!`);
+}
+else {
+    console.log(`Good night ${name}!`);
+}
+
+
+const age = 21;
+if (age <= 6 || age >= 65){
+    console.log(`Discount`);
+}else{
+    console.log(`NO discount`);
+}
+
+if (age <= 6 || age >= 65){
+    console.log(`cant eat much`);
+}else{
+    console.log(`Full price`);
+}
+
